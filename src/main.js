a76b16d5db07b65d402f712408b2f995d6c96420
Kamil Sztefko <sztefkokamil@gmail.com>
2019-05-15 10:51:13 +0200

    Install missing loaders & add test files
    Missing loaders: node-sass, @babel/core, @babel/preset-env
    
    Test files: index.html, main.js, /sass/style.scss

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import style from './sass/style.scss'
+
+window.onload = () => {
+
+  let handler = document.getElementById('text');
+  handler.textContent = 'Text added by JavaScript';
+
+  handler = document.getElementById('title');
+  let title = handler.textContent;
+  console.log(title);
+  handler.textContent = title.toUpperCase();  
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {presets: ['@babel/preset-env']}
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'images'
+        }
+      }
+    ]
+  }
+};
