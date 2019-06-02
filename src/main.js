b0e19f8190c64d313f5611b9e65ab8ce5fe130f0
Kamil Sztefko <sztefkokamil@gmail.com>
2019-06-02 09:51:02 +0200

    Remove dummy testing webpack content from main.js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import './sass/style.scss'
 
 window.onload = () => {
 
-  let handler = document.getElementById('text');
-  handler.textContent = 'Text added by JavaScript!!';
-
-  handler = document.getElementById('title');
-  let title = handler.textContent;
-  console.log(title);
-  handler.textContent = title.toUpperCase();  
+  console.log('Page loaded');
 
 }
