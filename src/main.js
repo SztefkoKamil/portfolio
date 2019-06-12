7efab1b5f3ef4dad1fd795dad7cb592f8946b661
Kamil Sztefko <sztefkokamil@gmail.com>
2019-06-12 20:35:16 +0200

    Add menu scripts
    Added menu file.
    Added first menu scripts part.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import './sass/style.scss'
 import './assets/Kamil-Sztefko-CV.pdf'
 
+import { menu } from './scripts/menu';
+
 window.onload = () => {
 
   console.log('Page loaded');
+
+  menu();
 
 }
---
+++ src/scripts/menu.js
@@ -0,0 +1,9 @@
+module.exports.menu = () => {
+  const menuBtn = document.getElementById('menu-toggle-btn'),
+        menuField = document.getElementById('menu-field');
+
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('menu__toggle-btn--open');
+    menuField.classList.toggle('nav__menu-field--open');
+  });
+}
