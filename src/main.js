3e302514e4b69e3924e9f05343160fabb5b3422c
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-02 12:44:38 +0200

    Switch skills tiles to web components

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 
 import './sass/style.scss';
 
+import './components/skill';
+
 import { activeScreenPositionDetector } from './scripts/screenPosition';
 import { activeMenuListeners } from './scripts/menuListeners';
 
