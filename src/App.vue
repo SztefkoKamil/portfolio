9cfef7a97e65069ca7ea03224f5d8d71563b2aa8
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-17 19:39:52 +0200

    Add CSS variables

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,17 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+// variables
+html {
+  --font-first: "Lato";
+  --font-second: "Josefin Sans";
+  --font-first: "Audiowide";
+  --color-first: #fafafa;
+  --first: #a825c9;
+  --bg-first: #0d0d0d;
+  --bg-second: #08000a;
+  --text-shadow: 2px 2px 2px var(--first);
+  --box-shadow: 0 0 15px var(--first);
 }
 
 *,
@@ -28,5 +33,19 @@ export default {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+}
+
+button {
+  border: none;
+  background: transparent;
+  color: currentColor;
+}
+
+#app {
+  font-family: "Lato", Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: var(--color-first);
 }
 </style>
