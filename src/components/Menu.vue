ed42eb253ab196264aa75f7651dac159ac844755
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-17 17:01:19 +0200

    Add menu with simple styles

---
+++ src/components/Menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="menu-container">
+    <button>MENU</button>
+    <div>
+      <span>Kamil Sztefko</span>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/">Intro</router-link>
+          </li>
+          <li>
+            <router-link to="/about">About</router-link>
+          </li>
+          <li>
+            <router-link to="/skills">Skills</router-link>
+          </li>
+          <li>
+            <router-link to="/projects">Projects</router-link>
+          </li>
+          <li>
+            <router-link to="/contact">Contact</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.menu-container {
+  padding: 10px;
+
+  button,
+  span {
+    display: none;
+  }
+
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
