7a74453cf41445e16386b51e3b8603b8331d6554
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-17 16:49:25 +0200

    Add routes for views

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,41 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Intro from "../views/Intro";
+import About from "../views/About";
+import Skills from "../views/Skills";
+import Projects from "../views/Projects";
+import Contact from "../views/Contact";
+
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    name: "Intro",
+    path: "/",
+    component: Intro
+  },
+  {
+    name: "About",
+    path: "/about",
+    component: About
+  },
+  {
+    name: "Skills",
+    path: "/skills",
+    component: Skills
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+    component: Projects
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+    component: Contact
+  }
+];
 
 const router = new VueRouter({
   mode: "history",
