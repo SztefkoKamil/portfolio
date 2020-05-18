12e51bace7d77cb452b837df7f893c40c7835ae3
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-18 08:41:04 +0200

    Complete Menu.vue
    Added unit tests.
    Added logic.
    Added styles with RWD.
    Added animations.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,44 +1,214 @@
 <template>
   <div class="menu-container">
-    <button>MENU</button>
-    <div>
-      <span>Kamil Sztefko</span>
-      <nav>
-        <ul>
-          <li>
-            <router-link to="/">Intro</router-link>
-          </li>
-          <li>
-            <router-link to="/about">About</router-link>
-          </li>
-          <li>
-            <router-link to="/skills">Skills</router-link>
-          </li>
-          <li>
-            <router-link to="/projects">Projects</router-link>
-          </li>
-          <li>
-            <router-link to="/contact">Contact</router-link>
-          </li>
-        </ul>
-      </nav>
+    <button
+      class="toggle-menu-btn"
+      :class="{ 'menu-open': isOpen }"
+      @click="isOpen = !isOpen"
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+    <div class="nav-wrapper" ref="wrapper">
+      <transition name="toggle-nav">
+        <div class="nav-container" v-if="isOpen">
+          <span class="nav-title">Kamil Sztefko</span>
+          <nav>
+            <ul>
+              <li @click="closeNav">
+                <router-link class="nav-btn" to="/" tag="button" exact
+                  >Intro</router-link
+                >
+              </li>
+              <li @click="closeNav">
+                <router-link class="nav-btn" to="/about" tag="button" exact
+                  >About</router-link
+                >
+              </li>
+              <li @click="closeNav">
+                <router-link class="nav-btn" to="/skills" tag="button" exact
+                  >Skills</router-link
+                >
+              </li>
+              <li @click="closeNav">
+                <router-link class="nav-btn" to="/projects" tag="button" exact
+                  >Projects</router-link
+                >
+              </li>
+              <li @click="closeNav">
+                <router-link class="nav-btn" to="/contact" tag="button" exact
+                  >Contact</router-link
+                >
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  watch: {
+    isOpen(value) {
+      if (value) this.$refs.wrapper.classList.add('menu-open');
+      else {
+        setTimeout(() => {
+          this.$refs.wrapper.classList.remove('menu-open');
+        }, 300);
+      }
+    }
+  },
+
+  methods: {
+    closeNav() {
+      this.isOpen = false;
+    }
+  }
+};
+</script>
+
 <style lang="scss">
 .menu-container {
-  padding: 10px;
+  position: relative;
+  z-index: 100;
 
-  button,
-  span {
-    display: none;
+  .toggle-menu-btn {
+    width: 80px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--bg-first);
+    position: fixed;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 20;
+
+    span {
+      width: 14px;
+      height: 14px;
+      border-radius: 50%;
+      border: 5px solid var(--first);
+      transition: border-width 0.3s ease-in, transform 0.3s ease-in;
+
+      &:nth-child(2) {
+        margin: 0 4px;
+      }
+    }
+
+    &.menu-open {
+      span {
+        border-width: 2px;
+        transform: scale(1.3);
+      }
+    }
   }
 
-  ul {
-    list-style: none;
+  .nav-wrapper {
+    perspective: 500px;
+    perspective-origin: 50% 42%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 0;
+    padding: 80px 0 30px;
+    overflow: auto;
+
+    &.menu-open {
+      height: 100vh;
+    }
+  }
+
+  .nav-container {
     display: flex;
-    justify-content: space-between;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
+    background-color: hsla(0, 0%, 0%, 10%);
+
+    .nav-title {
+      font-family: var(--font-third);
+      font-size: 30px;
+      text-align: center;
+      text-shadow: var(--text-shadow);
+    }
+
+    ul {
+      list-style: none;
+      margin: 50px 0 0;
+    }
+
+    li {
+      margin: 0 0 20px;
+    }
+
+    .nav-btn {
+      font-size: 30px;
+      text-shadow: var(--text-shadow);
+      text-align: center;
+      line-height: 100%;
+      width: 220px;
+      padding: 5px 0 8px;
+      border: 1px solid transparent;
+      transition: border-color 0.1s ease-in;
+
+      &:hover,
+      &.router-link-active {
+        border-color: var(--first);
+      }
+    }
+
+    @media screen and (min-width: 1024px) {
+      & {
+        padding-top: 50px;
+      }
+
+      .nav-title {
+        font-size: 40px;
+        letter-spacing: 2px;
+      }
+
+      .nav-btn {
+        font-size: 40px;
+        letter-spacing: 2px;
+      }
+    }
+  }
+
+  .toggle-nav-enter-active {
+    animation: show-nav 0.3s 1 ease-in;
+  }
+  @keyframes show-nav {
+    0% {
+      transform: translateZ(400px) scale(0.5);
+      opacity: 0;
+    }
+    100% {
+      transform: translateZ(0px) scale(1);
+      opacity: 1;
+    }
+  }
+  .toggle-nav-leave-active {
+    animation: hide-nav 0.3s 1 ease-in;
+  }
+  @keyframes hide-nav {
+    0% {
+      transform: translateZ(0px) scale(1);
+      opacity: 1;
+    }
+    100% {
+      transform: translateZ(400px) scale(0.5);
+      opacity: 0;
+    }
   }
 }
 </style>
