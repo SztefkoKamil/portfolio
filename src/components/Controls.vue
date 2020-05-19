d3b84016f3b1bc1f0493758542c61c5316de7c03
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-19 08:19:50 +0200

    Add touch navigation feature to Controls
    Added spam click blocker feature.

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="controls-container">
+  <nav class="controls-container" ref="controlsContainer">
     <button class="left" @click="prevRoute">
       <svg
         aria-hidden="true"
@@ -11,7 +11,7 @@
         <path
           fill="currentColor"
           d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
-        ></path>
+        />
       </svg>
     </button>
     <button class="right" @click="nextRoute">
@@ -25,7 +25,7 @@
         <path
           fill="currentColor"
           d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
-        ></path>
+        />
       </svg>
     </button>
     <button v-if="showDown" @click="scrollDown" class="down">
@@ -39,7 +39,7 @@
         <path
           fill="currentColor"
           d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"
-        ></path>
+        />
       </svg>
     </button>
   </nav>
@@ -51,10 +51,12 @@ import eventBus from '../misc/eventBus';
 export default {
   data() {
     return {
-      showDown: false
+      showDown: false,
+      spamClickBlocker: false
     };
   },
-  created() {
+  mounted() {
+    this.listenTouchMove();
     eventBus.$on('showDown', () => {
       this.showDown = true;
     });
@@ -64,18 +66,50 @@ export default {
   },
   methods: {
     prevRoute() {
+      if (this.spamClickBlocker) return null;
+      this.spamClickBlocker = true;
+      // listeners: /router/logic.js
+      eventBus.$emit('controlClick');
       // listeners: App.vue
       eventBus.$emit('prevRoute');
       this.$router.prevRoute();
+      setTimeout(() => (this.spamClickBlocker = false), 700);
     },
     nextRoute() {
+      if (this.spamClickBlocker) return null;
+      this.spamClickBlocker = true;
+      // listeners: /router/logic.js
+      eventBus.$emit('controlClick');
       // listeners: App.vue
       eventBus.$emit('nextRoute');
       this.$router.nextRoute();
+      setTimeout(() => (this.spamClickBlocker = false), 700);
     },
     scrollDown() {
       // listeners: About.vue
       eventBus.$emit('scrollDown');
+    },
+    listenTouchMove() {
+      const controlPanelHeight = 120;
+      const moveMin = 50;
+      let start = null;
+      this.$refs.controlsContainer.addEventListener('touchstart', (event) => {
+        // console.log(event.targetTouches[0]);
+        start = event.targetTouches[0];
+      });
+      this.$refs.controlsContainer.addEventListener('touchend', (event) => {
+        const windowHeight = window.innerHeight;
+        // console.log(event.changedTouches[0]);
+        const end = event.changedTouches[0];
+        const startHeight =
+          windowHeight - start.pageY < controlPanelHeight ? true : false;
+        const endHeight =
+          windowHeight - end.pageY < controlPanelHeight ? true : false;
+        if (startHeight && endHeight && start.pageX - moveMin > end.pageX)
+          this.nextRoute();
+        else if (startHeight && endHeight && start.pageX + moveMin < end.pageX)
+          this.prevRoute();
+      });
     }
   }
 };
