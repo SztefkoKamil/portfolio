47e41e9e3eacd69a6187cf65d52b12fb05f920ee
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-19 08:38:33 +0200

    Add view switch animation in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Menu />
-    <transition name="switch-view">
+    <transition :name="transitionDirection">
       <keep-alive>
         <router-view />
       </keep-alive>
@@ -12,11 +12,21 @@
 </template>
 
 <script>
+import eventBus from './misc/eventBus';
 import Menu from './components/Menu';
 import Controls from './components/Controls';
 
 export default {
-  components: { Menu, Controls }
+  components: { Menu, Controls },
+  data() {
+    return {
+      transitionDirection: 'next-view'
+    };
+  },
+  created() {
+    eventBus.$on('nextRoute', () => (this.transitionDirection = 'next-view'));
+    eventBus.$on('prevRoute', () => (this.transitionDirection = 'prev-view'));
+  }
 };
 </script>
 
@@ -24,9 +34,12 @@ export default {
 @import './misc/global-styles.scss';
 
 #app {
-  min-height: 100vh;
-  min-height: -webkit-fill-available;
-  padding: 90px 0 0;
+  height: 100vh;
+  max-height: -webkit-fill-available;
+  max-width: 960px;
+  overflow: hidden;
+  position: relative;
+  margin: 0 auto;
 
   .background {
     background: url('./assets/images/bg.webp') center top;
@@ -38,6 +51,65 @@ export default {
     right: 0;
     bottom: 0;
     z-index: -10;
+  }
+}
+
+// view switch animation
+#app {
+  //animation timers
+  --switch-time: 0.3s;
+  --switch-delay: 0.2s;
+
+  // next-view animations
+  .next-view-enter-active {
+    animation: next-show var(--switch-time) var(--switch-delay) 1 ease-in-out
+      backwards;
+  }
+  @keyframes next-show {
+    0% {
+      transform: translateX(120vw);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  .next-view-leave-active {
+    animation: next-hide var(--switch-time) var(--switch-delay) 1 ease-in-out;
+  }
+  @keyframes next-hide {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-120vw);
+    }
+  }
+
+  // prev-view animations
+  .prev-view-enter-active {
+    animation: prev-show var(--switch-time) var(--switch-delay) 1 ease-in-out
+      backwards;
+  }
+  @keyframes prev-show {
+    0% {
+      transform: translateX(-120vw);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+
+  .prev-view-leave-active {
+    animation: prev-hide var(--switch-time) var(--switch-delay) 1 ease-in-out;
+  }
+  @keyframes prev-hide {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(120vw);
+    }
   }
 }
 </style>
