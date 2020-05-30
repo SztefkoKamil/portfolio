1d4a90089e931f42e56b47187c0e4a540e97eb62
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-30 11:41:08 +0200

    Add moduleNameMapper to Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  testMatch: ['**/?(*.)(spec).js?(x)', '**/?(*.)(test).js?(x)']
+  testMatch: ['**/?(*.)(spec).js?(x)', '**/?(*.)(test).js?(x)'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>/assetsTransformer.js'
+  }
 };
