8df498a5776722dc9b5ce8a8700c39604875cee5
Kamil Sztefko <sztefkokamil@gmail.com>
2020-06-01 15:58:43 +0200

    Add unit tests for Projects.vue

---
+++ test/unit/views/Projects.unit.test.js
@@ -0,0 +1,19 @@
+import { shallowMount } from '@vue/test-utils';
+
+import Projects from '../../../src/views/Projects';
+
+describe('/components/Projects.vue', () => {
+  it('should comprise h1', () => {
+    const wrapper = shallowMount(Projects);
+    const h1 = wrapper.find('h1');
+
+    expect(h1.exists()).toBe(true);
+  });
+
+  it('should has projects property which returns Array', () => {
+    const wrapper = shallowMount(Projects);
+    const { projects } = wrapper.vm;
+
+    expect(projects).toEqual(expect.any(Array));
+  });
+});
